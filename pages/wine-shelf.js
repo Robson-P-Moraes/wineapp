cce0c2f3b6c72b203041e2ef29b5e0d9f33b4da5
Robson-P-Moraes <robsonpdemoraes.dev@hotmail.com>
2024-10-28 07:09:22 -0300

    Atuliazação da página create.js e Criação da página wine-shelf

---
+++ pages/wine-shelf.js
@@ -0,0 +1,44 @@
+// Página de exibição dos vinhos
+
+import { useEffect, useState } from "react";
+import {isAuthenticated} from '../utils/auth';
+import { useRouter } from "next/router";
+
+export default function WineAhelf() {
+    const [wines, setWines] = useState([]);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!isAuthenticated()){
+            router.push('/login');
+            return;
+        }
+
+        const fetWines = async() => {
+            const token = localStorage.getItem('token');
+            const response = await fetch('/api/wines', {
+                headers: {'Authorization': `Bearer ${token}`},
+            });
+            const data = await response.json();
+            setWines(data);
+        };
+
+        fetchWines();
+    }, []);
+
+    return (
+        <div>
+            <h1>Minha coleção de Vinhos</h1>
+
+            <ul>
+                {wines.map((wine) => {
+                    <li key={wine._id}>
+                        <h2>{wine.name}</h2>
+                        <p>{wine.type} - {wine.year}</p>
+                        <p>Melhor ano para consumo: {wine.bestYearToConsume}</p>
+                    </li>
+                })}
+            </ul>
+        </div>
+    );
+}
